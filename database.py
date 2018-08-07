46f08f8bbd76b6fa3b2b5b9a7f96775afca26ffe
Unknown <nicnick1989@gmail.com>
2018-08-07 12:06:35 +1000

    wrote python script for database

---
+++ database.py
@@ -0,0 +1,7 @@
+import sqlite3 as lite
+import sys
+connection = lite.connect('sensehat.db')
+with connection:
+    cur = connection.cursor()
+    cur.execute("DROP TABLE IF EXISTS SENSEHAT_data")
+    cur.execute("CREATE TABLE SENSEHAT_data(timestamp DATETIME, temp NUMERIC, humidity NUMERIC")
