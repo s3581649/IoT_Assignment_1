a9d008e7d56a59a7553d1cd4d67b8611751d4bd3
Noor Sabbagh <noorsabbagh77@gmail.com>
2018-08-08 15:31:34 +1000

    Fixed cronJob

--- cron_job.py
+++ cron_job.py
@@ -6,7 +6,7 @@ cron = CronTab(user='pi')
 cron.remove_all()
 
 #add new cron job
-job  = cron.new(command='/home/pi/Sensors_Database/sense_data.py')
+job  = cron.new(command='/home/pi/Sensors_Database/senseData_App.py')
 
 #job settings
 job.minute.every(2)
